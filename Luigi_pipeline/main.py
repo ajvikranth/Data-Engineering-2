899490a758ef36c529f751663ad9a21fa8e64a9a
ajvikranth <114017744+ajvikranth@users.noreply.github.com>
2024-06-07 00:18:56 +0200

    running luigi with local scheduler

--- Luigi_pipeline/main.py
+++ Luigi_pipeline/main.py
@@ -3,9 +3,10 @@ import os
 from tasks.track_info_fetch import fetch_track_info
 
 if __name__ =="__main__":
-    # luigi.run(['fetch_track_info', '--local-scheduler'])
+    
     try:
         os.remove('.cache')
     except OSError:
         pass
-    luigi.run(['fetch_track_info'])
+    # luigi.run(['fetch_track_info'])
+    luigi.run(['fetch_track_info', '--local-scheduler'])
